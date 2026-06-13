bf2712270d6982f7fd7a9570901e7a485a4f67a2
ChangOh Kim <kco9077@gmail.com>
2026-06-13 16:13:39 +0900

    style: 웰컴 화면 UI/UX 업데이트
    what:
    - WelcomeScreen 디자인 변경 및 테마 적용
    
    impact:
    - presentation 모듈의 WelcomeScreen 화면 및 구성 파일
    
    closes: #56

--- presentation/src/main/java/kr/co/presentation/theme/Type.kt
+++ presentation/src/main/java/kr/co/presentation/theme/Type.kt
@@ -14,21 +14,19 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+    displayLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = (-0.8).sp
+    ),
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+        fontSize = 18.sp,
+        lineHeight = 29.25.sp,
         letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
     )
-    */
 )
